7c1cea8c38487dcf734ebf7d087488d366e512c2
pramirez4 <59981840+pramirez4@users.noreply.github.com>
2020-03-10 20:02:00 -0500

    Create FileAccessor.cs

---
+++ SummarizeTempsObjects/EngineTests/FileAccessor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace SummarizeTempsObjects
+{
+    public class FileAccessor
+    {
+        string _filePath = "";
+
+        public List<int> Temperatures = new List<int>();
+        public FileAccessor(string filePath)
+        {
+            _filePath = filePath;
+
+        }
+
+        public void LoadTemperatureData()
+        {
+            if (File.Exists(_filePath))
+            {
+                using (StreamReader sr = File.OpenText(_filePath))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        // convert the string temp to an int
+                        int temp = int.Parse(line);
+
+                        // add to the ArrayList
+                        Temperatures.Add(temp);
+                    }
+                }
+            }
+        }
+    }
+}
